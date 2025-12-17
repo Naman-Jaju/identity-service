645ee97609acefb43b58939d14a0f562c054b78e
Naman <namanjaju2004@gmail.com>
2025-12-17 22:42:54 +0530

    Initial setup

---
+++ src/types/types.ts
@@ -0,0 +1,44 @@
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface AccessToken {
+    sub: string;
+    email: string;
+    iat: number;
+    exp: number;
+    type: 'access';
+}
+
+export interface UserDTO {
+  id: string;
+  email: string;
+}
+
+
+export interface RefreshToken {
+    sub: string;
+    iat: number;
+    exp: number;
+    type: 'refresh';
+}
+
+export interface RegisterRequest {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface AuthResponse {
+  accessToken: string;
+  user: UserDTO;
+}
